126d599be009bba2c042631a247f6fe7cd9fb659
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-01-11 18:55:34 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Johari::Application.config.secret_key_base = '94f8b22ac63de2c1b12b45e45ccf21a0436ca1c694e85d93b03211deb5c43fdf0441414f3e0cea7efb07858603b8cab9cb0c94287cc4719d2efba01de29e21f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Johari::Application.config.session_store :cookie_store, key: '_Johari_session'
