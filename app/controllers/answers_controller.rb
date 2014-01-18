81f0da37360a1d04b03d217eb5b3d81551f6db34
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-01-18 18:00:32 +0100

    Detecting multiple adjectives separated by commas

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,15 +8,18 @@ class AnswersController < ApplicationController
   def show
   end
   def create
+    words = params[:post][:Adjectives].split(",")
+    words.each do |word|
       case params[:post][:LOA]
-      when "High"
-        Answer.find_or_create_by_Word(params[:post][:Adjectives]).increment!(:LOA_High)
-      when "Medium"
-        Answer.find_or_create_by_Word(params[:post][:Adjectives]).increment!(:LOA_Med)
-      when "Low"
-        Answer.find_or_create_by_Word(params[:post][:Adjectives]).increment!(:LOA_Low)
+        when "High"
+          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
+        when "Medium"
+          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
+        when "Low"
+          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
       end
-    Answer.find_or_create_by_Word(params[:post][:Adjectives]).increment!(:Occur)
+      Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
+    end
     redirect_to answers_path
   end
 end
