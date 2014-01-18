ac0094564626e891da3c55a1da2af7625e13ef36
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-01-18 21:49:19 +0100

    Showing existing answers and being able to select from those as well as write your own

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,18 +8,19 @@ class AnswersController < ApplicationController
   def show
   end
   def create
-    words = params[:post][:Adjectives].split(",")
-    words.each do |word|
-      case params[:post][:LOA]
-        when "High"
-          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
-        when "Medium"
-          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
-        when "Low"
-          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
+    params[:answer][:Adjectives].each do |set|
+      set.split(",").each do |word|
+        case params[:answer][:LOA]
+          when "High"
+            Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
+          when "Medium"
+            Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
+          when "Low"
+            Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
+        end
+        Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
       end
-      Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
     end
-    redirect_to answers_path
+    render text: params[:answer]
   end
 end
