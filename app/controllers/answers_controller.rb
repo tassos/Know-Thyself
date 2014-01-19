8f586f2cfac22855fe95c55c9019c12fc55c2244
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-01-19 16:04:28 +0100

    Fixed problem with detecting if a word is entered twice. Also better string splitting for the text entry

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,18 +8,23 @@ class AnswersController < ApplicationController
   def show
   end
   def create
-    params[:answer][:Adjectives].uniq.each do |set|
-      set.split(",").each do |word|
-        case params[:answer][:LOA]
-          when "High"
-            Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
-          when "Medium"
-            Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
-          when "Low"
-            Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
-        end
-        Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
+    filtered=[]
+    params[:answer][:Adjectives].each do |words|
+      words.split( /, */ ).uniq.each do |word|
+        puts filtered << word.downcase
       end
+    end
+        
+    filtered.uniq.each do |word| 
+      case params[:answer][:LOA]
+        when "High"
+          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
+        when "Medium"
+          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
+        when "Low"
+          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
+      end
+      Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
     end
     redirect_to answers_path
   end
