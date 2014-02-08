bf0d642373b67743673da89687430a49f75c21ca
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-02-08 16:24:44 +0100

    Validating if the user actually selected any adjectives and LOA. If not, return to the new answer page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,23 +9,26 @@ class AnswersController < ApplicationController
   end
   def create
     filtered=[]
-    params[:answer][:Adjectives].each do |words|
-      words.split( /, */ ).uniq.each do |word|
-        puts filtered << word.downcase
+    if params[:answer][:Adjectives].join.empty?
+      redirect_to new_answer_path
+    elsif params[:answer][:LOA].empty?
+      redirect_to new_answer_path
+    else
+      params[:answer][:Adjectives].each do |words|
+        words.split( /, */ ).uniq.each do |word|
+          filtered << word.downcase
+        end
       end
-    end
-        
-    filtered.uniq.each do |word| 
-      case params[:answer][:LOA]
-        when "1"
-          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
-        when "2"
-          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
-        when "3"
-          Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
+  
+      filtered.uniq.each do |word|
+        case params[:answer][:LOA]
+          when "1" then Answer.find_or_create_by_Word(word.strip).increment!(:LOA_High)
+          when "2" then Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Med)
+          when "3" then Answer.find_or_create_by_Word(word.strip).increment!(:LOA_Low)
+        end
+        Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
       end
-      Answer.find_or_create_by_Word(word.strip).increment!(:Occur)
+      redirect_to answers_path
     end
-    redirect_to answers_path
   end
 end
