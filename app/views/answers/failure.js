ee27b83c18704c1f3745e64aee676cbec58d059e
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-03-02 00:07:06 +0100

    Styling the error message to look pretty

--- app/views/answers/failure.js
+++ app/views/answers/failure.js
@@ -1,1 +1,8 @@
-$('#new_answer').before('<% flash.each do |key, value| %><div id="flash <%= key %> alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><%= value%></div><%end%>');
+$('#new_answer').before(
+  '<% flash.each do |key, value| %>\
+    <div id="flash <%= key %>" class="alert alert-danger alert-dismissable">\
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
+      <%= value%>\
+    </div>\
+  <%end%>'
+);
