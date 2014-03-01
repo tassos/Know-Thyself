5d6ba16d579ad6be95074bf034984ddd8b3c8d96
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-03-02 00:52:45 +0100

    Made possible to remove old alerts and update them with new ones

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 //= require_tree .
 
 jQuery.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript");}
+});
 
 jQuery.fn.submitWithAjax = function() {
   this.submit(function() {
     $.post(this.action, $(this).serialize(), null, "script");
     return false;
-  })
+  });
   return this;
 };
 
 $(document).ready(function() {
   $("#new_answer").submitWithAjax();
-})
+});
--- app/views/answers/failure.js
+++ app/views/answers/failure.js
@@ -1,6 +1,8 @@
+$('#flash').remove();
+
 $('#new_answer').before(
   '<% flash.each do |key, value| %>\
-    <div id="flash <%= key %>" class="alert alert-danger alert-dismissable">\
+    <div id="flash" class="alert alert-danger alert-dismissable fade in">\
       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
       <%= value%>\
     </div>\
