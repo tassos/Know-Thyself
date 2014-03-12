8c83b2511d961e1b013746fe655dcc89cd52c255
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-03-12 20:42:11 +0100

    Creating devise models for users and admins

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '4.0.1'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
+# Use Devise for user authentication
+gem 'devise'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Johari::Application.routes.draw do
+  devise_for :admins
+  devise_for :users
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
