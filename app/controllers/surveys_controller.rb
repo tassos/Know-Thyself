e34c5604180fd6cecf27ce062e08a2add0c33b17
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-05-01 16:43:02 +0200

    Creating a new survey and storing the response of the creator

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,22 +2,30 @@ class SurveysController < ApplicationController
   before_filter :authenticate_user!
   def new
     @survey = Survey.new
-    @self_response = SelfResponse.new
+    @response = Response.new
     @adjectives = Adjective.all.order(:word)
   end
   def create
-    
-    @survey.user_id = current_user.id
-    @survey.save
-
-
-    @selfResponse.surve_id = @survey.id
-    @selfResponse.save
-    
-    redirect_to survey_index_path
-    
+    if params[:response][:Adjectives].empty?
+      flash[:notice]="Please fill in some Adjectives that describe me!"
+      respond_to do |format|
+        format.html { redirect_to new_answer_path }
+        format.js {render :action => 'failure'}
+      end
+    else
+      @survey = Survey.new(user_id: current_user.id)
+      @survey.save
+  
+      @response = Response.new(survey_id: @survey.id, loa: '4', adjective_ids: params[:response][:Adjectives])
+      @response.save
+      
+      redirect_to surveys_path
+    end
   end
   def index
     @surveys=current_user.surveys
+  end
+  def show
+   
   end
 end
