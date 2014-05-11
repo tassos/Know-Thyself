4db83147e3db0a33dee67a5fcab11ff720e82bc5
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-05-11 15:25:18 +0200

    Fixing some remaining problems with names in singular/plural

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AnswersControllerTest < ActionController::TestCase
+class Users::SessionsControllerControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
---
+++ test/helpers/users/registrations_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Users::SessionsControllerHelperTest < ActionView::TestCase
+end
