b48e8b6713f69908c5624367e55cede0a135752e
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-06-15 20:59:42 +0200

    Adding an 'about' page for explanations about the application

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Johari::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/about' => 'welcome#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
