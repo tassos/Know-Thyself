924dc587772a41aed16ef3c98733a8464ea7081d
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-06-20 00:10:06 +0200

    Allowing a user to remove his/her e-mail address from the newsletter

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,17 @@
 class AddressesController < ApplicationController
   def create
-    @address = Address.new(email: params[:address][:email])
-    if @address.valid?
-      @address.save
-      flash[:notice]="Thank you for subscribing to the newsletter!"
-    else
-      flash[:warning]="You are already subscribed to the newsletter."
+    if params[:add_button]
+      @address = Address.new(email: params[:address][:email])
+      if @address.valid?
+        @address.save
+        flash[:notice]="Thank you for subscribing to the newsletter!"
+      else
+        flash[:warning]="You are already subscribed to the newsletter."
+      end
+    elsif params[:remove_button]
+      @address = Address.find_by_email(params[:address][:email])
+      @address.destroy
+      flash[:notice]="You are now unsubscribed from the newsletter"
     end
      
     redirect_to root_path
