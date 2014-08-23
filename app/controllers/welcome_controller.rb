387234fe5c77bc705a173bd5b29bea70305a7e24
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-08-23 20:48:16 +0200

    Showing the name of the user in the welcome page, if the user is signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @address = Address.new
+    if user_signed_in?
+      @salutation = current_user.name
+    else
+      @salutation = 'guest'
+    end
   end
 end
