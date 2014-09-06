a3b2f87a42fba084c76e8e4ae9578680ec6863d2
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-09-06 22:19:07 +0200

    Adding a devise_parameters_sanitizer for the updating of account information, so that the users can change their name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name) }
   end
 end
