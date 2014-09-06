9d06a368ba58062fe0bf3803431311eab15719b6
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-09-06 21:56:47 +0200

    Refactored adjectives controller to avoid dublicate code

--- app/controllers/adjectives_controller.rb
+++ app/controllers/adjectives_controller.rb
@@ -6,22 +6,20 @@ class AdjectivesController < ApplicationController
   end
   
   def create
-    unless params[:answer][:toRemove].nil?
-      params[:answer][:toRemove].uniq.each do |word|
-        @adjective=Adjective.find_by_word(word.downcase)
-        @adjective.visibility = 0
-        @adjective.save
-      end
-    end
-    
-    unless params[:answer][:toAdd].nil?
-      params[:answer][:toAdd][0].split( /, */ ).uniq.each do |word|
+    update(params[:answer][:toRemove],0)    
+    update(params[:answer][:toAdd],1)
+    redirect_to adjectives_path
+  end
+  
+  private
+  def update(parameters,visibility)
+    unless parameters.nil?
+      parameters[0].split( /, */ ).uniq.each do |word|
         Adjective.find_or_create_by_word(word.downcase)
         @adjective=Adjective.find_by_word(word.downcase)
-        @adjective.visibility = 1
+        @adjective.visibility = visibility
         @adjective.save
       end
     end
-  redirect_to adjectives_path
   end
 end
