00477db6badea40d9555bd6423b05ff241aab5b8
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-12-20 19:22:30 +0100

    Setting the 'registered' value to true for new users at sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
   end
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :registered) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name) }
   end
 end
